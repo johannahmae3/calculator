a7cbd23c79b4264b95b0dd564afe0c0f35541025
johannahmae3 <johannahmae19@gmail.com>
2018-08-13 10:09:50 +0800

    joana commit

--- javascript.js
+++ javascript.js
@@ -1,13 +1,42 @@
-function insert(number){
- document.form.total.value = document.form.total.value+number;
+var form = document.querySelectorAll(".number");
+var sign = document.querySelectorAll(".sign");
+var input = document.querySelector("#total");
+var clear = document.querySelector(".clear");
+var equal = document.querySelector(".quant");
+var del = document.querySelector(".remove");
+for (var i = 0; i < form.length; i++)
+{
+  form[i].addEventListener("click", function()
+  {
+    input.value += this.value; 
+
+     console.log(this.value)
+ 
+  });
 }
-function equal1(){
-    document.form.total.value = eval(document.form.total.value);
+for (var x = 0; x < sign.length; x++){
+  sign[x].addEventListener("click", function()
+{
+      input.value += this.value;
+      console.log(this.value)
+  
+  });
 }
-function clear1(){
-    document.form.total.value = "";
-}
-function del(){
-     var exp = document.form.total.value;
-     document.form.total.value = exp(0,exp.length-1)
- }
+equal.addEventListener("click", function(){
+  input.value = eval(input.value);
+});
+clear.addEventListener("click", function(){
+  input.value = "";
+});
+del.addEventListener("click", function(){
+  input.value = input.value.substring(0,input.value.length-1);
+});
+
+
+
+
+
+ 
+console.log(form);
+
+
